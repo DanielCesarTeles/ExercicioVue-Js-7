f50b49927c7acd35ac37e1f31028a2c6368acdb6
DanielCesarTeles <daniel.cesarteles@gmail.com>
2020-06-26 16:58:18 -0300

    First commit ExercicioVue-Js-7

---
+++ ExercicioVue-Js-7/main.js
@@ -0,0 +1,11 @@
+var vue = new Vue({
+  el:"#app",
+  data : {
+    meses : [ "Janeiro","Fevereiro","Março","Abril","Maio","Junho","Julho",
+              "Agosto","Setembro","Outubro","Novembro","Dezembro"]
+  },
+  computed: {}
+  ,
+  methods: {}
+  }
+);
